02ffda5bb62c80f642b90e5eac5b9b7a2507af01
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2025-12-22 21:02:42 +0100

    fix: dark/light logo when overriding system dark mode

--- eleventy.config.js
+++ eleventy.config.js
@@ -25,6 +25,7 @@ export default function (config) {
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/*.png");
     config.addPassthroughCopy("src/favicon.*");
+    config.addPassthroughCopy("src/favicon-*.*");
     config.addPassthroughCopy("src/files");
 
     config.addTemplateFormats('css');
